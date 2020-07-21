e00db6d07a62171143a87850d5e0a1216e70d3ef
aslikr211234 <arturo211234@gmail.com>
2020-07-20 20:03:04 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #define FIZZNUM 3
 #define BUZZNUM 5
-#define MAXLENGTH 8
-#define B 66
-#define F 70
-#define I 73
-#define U 85
-#define Z 90
+#define MAXLENGTH 10
 
+
+char giveLetter(int inp){
+ return ((inp == 1) * 70) + (((inp == 2) * 1) * 73) + ((inp == 3) * 66) + ((inp == 4) * 85) + ((inp == 5)*90);
+
+}
 
 
 int main(){
@@ -15,8 +15,8 @@ int main(){
  output[0] = "";
  int current = 0;
  for (int i = 0; i <= 100; i++){
-     if(i % FIZZNUM == 0 && i != 0) {output[current] = F; output[current+1] = I; output[current+2] = Z; output[current+3] = Z; current +=4;}
-     if(i % BUZZNUM == 0 && i !=0) {output[current] = B; output[current+1] = U; output[current+2] = Z; output[current+3] = Z; current +=4;}
+     if(i % FIZZNUM == 0 && i != 0) {output[current] = giveLetter(1); output[current+1] = giveLetter(2); output[current+2] = giveLetter(5); output[current+3] = giveLetter(5); current +=4;}
+     if(i % BUZZNUM == 0 && i != 0) {output[current] = giveLetter(3); output[current+1] = giveLetter(4); output[current+2] = giveLetter(5); output[current+3] = giveLetter(5); current +=4;}
      output[current] = '\n'; output[current+1]='\0';
      if(current == 0) {printf("%d", i);}
      printf("%s", output);
